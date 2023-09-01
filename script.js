7294bf3d07d9d1809559961bc1aee9ba5b5fac74
basit333 <shahbasit741@gmail.com>
2023-09-02 01:29:37 +0500

    made testimonials section

---
+++ script.js
@@ -0,0 +1,10 @@
+/*  Swiper Slider   */
+var swiper = new Swiper(".testimonialSlider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination.testimonial-pagination",
+    clickable: true,
+  },
+});
