f6edc1ee1e2a1de20cbf89643d5549eccaeb5039
basit333 <shahbasit741@gmail.com>
2023-09-05 03:02:29 +0500

    made desktop design

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ var swiper = new Swiper(".testimonialSlider", {
     clickable: true,
   },
 });
+
+/*  Tab Components   */
+const tabsBtn = document.querySelectorAll(".tab-btn");
+const tabContent = document.querySelectorAll(".tab-content-box");
+
+tabsBtn.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // Remove active class from all tabs and tab content
+    tabsBtn.forEach((tab) => tab.classList.remove("tab-btn-active"));
+    tabContent.forEach((content) => content.classList.remove("tab-content-box-active"));
+
+    // Add active class to the clicked tab
+    tab.classList.add("tab-btn-active");
+
+    // Show the corresponding tab content with opacity animation
+    const tabId = tab.getAttribute("data-tab");
+    const activeTabContent = document.getElementById(tabId);
+    activeTabContent.classList.add("tab-content-box-active");
+  });
+});
